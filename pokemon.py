afdea273b91e7f5d637554de64048eca094090a8
Scoutdrago3 <Drago7654321@gmail.com>
2015-09-05 15:22:36 -0400

    test

--- pokemon.py
+++ pokemon.py
@@ -6,3 +6,5 @@ pokemon = {
     'Bulbasaur':{'name':'Bulbasaur', 'health':45, 'attack':49, 'defense':49, 'spAttack':65, 'spDefense':65, 'speed':45, 'evoNum':16, 'num':1} #need all stats (ev_yield, catch rate, etc...)
     
 }
+
+print "github test"
